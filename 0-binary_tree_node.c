3c6c85f19200ccf111a4371cac7c8568f22c8c42
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-15 11:13:07 +0200

    adding "e"

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nod - function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  * Return: pointer to the new node, or NULL on failure
