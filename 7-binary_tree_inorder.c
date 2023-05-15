607a4e73d94628a44527abe0b72cc39b46b51b9d
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-15 14:42:50 +0200

    fix betty errors

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,13 +5,13 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: point function to call each node
  * Return: Nothing
-*/
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
 	binary_tree_inorder(tree->left, func);
-    (func)(tree->n);
+	(func)(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
