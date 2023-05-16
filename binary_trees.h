76bd4d4d0638d54d981447ee71705234ac070e26
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-16 10:53:11 +0200

    update header with new prototype function for task 17

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,5 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
