43a78a6a2b29a5f3753b9e42496a0de6140a32a6
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2023-05-18 01:19:13 +0200

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -28,6 +28,6 @@ int  binary_tree_is_full(const, const binary_tree_t *tree)
     
     if (tree->left != NULL && tree->right != NULL)
     {
-        return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
     }
 }
