60029e014a06375af204bad3645a2afdd1136a06
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2023-05-17 11:28:57 +0200

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,13 +10,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+     size_t count_left;
+     size_t count_right;
+    
     if (tree == NULL)
     {
         return 0;
     }
 
-    size_t count_left = binary_tree_nodes(tree->left);
-    size_t count_right = binary_tree_nodes(tree->right);
+    count_left = binary_tree_nodes(tree->left);
+    count_right = binary_tree_nodes(tree->right);
 
     if (tree->left != NULL || tree->right != NULL)
     {
