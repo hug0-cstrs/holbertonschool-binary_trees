ff9283c89b8528ea327d80d73475c71d51083e40
truuue <noahvernhet@gmail.com>
2023-05-19 11:09:48 -0700

    Fix compilation error for task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,32 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
-/** 
+/**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: the node
  * Return: 1 if the tree or 0 if  full
  */
-
-int binary_tree_is_full(const, const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (1);
-    }
-    
-    if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
-    {
-        return (0);
-    }
-    
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    
-    if (tree->left != NULL && tree->right != NULL)
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
+	if (tree == NULL)
+	{
+		return (1);
+	}
+
+	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
 }
