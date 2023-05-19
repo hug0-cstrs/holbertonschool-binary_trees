03ee05e1c10a42ccb86fde22975fba8762c323d2
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-19 20:32:11 +0200

    change return value from 1 to 0 in case : tree = NULL

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return (1);
+		return (0);
 	}
 
 	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
