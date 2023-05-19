6b4cea8cd7eca2ee944227ab55915c9ef1021296
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-19 20:35:53 +0200

    fix betty errors

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+	if ((tree->left == NULL && tree->right != NULL) ||
+	    (tree->left != NULL && tree->right == NULL))
 	{
 		return (0);
 	}
@@ -27,5 +28,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
 
-    return (0);
+	return (0);
 }
