f343b02a8a3c9505fe62698be7f18c52d4d263f9
truuue <noahvernhet@gmail.com>
2023-05-19 11:01:27 -0700

    Fix betty coding style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,32 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @tree: the node 
+ * binary_tree_nodes - Binary tree node
+ * @tree: the node
+ * Return: the number of nodes in a binary tree
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-     size_t count_left;
-     size_t count_right;
-    
-    if (tree == NULL)
-    {
-        return 0;
-    }
+	size_t count_left;
+	size_t count_right;
 
-    count_left = binary_tree_nodes(tree->left);
-    count_right = binary_tree_nodes(tree->right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        return 1 + count_left + count_right;
-    }
-    else
-    {
-        return count_left + count_right;
-    }
+	count_left = binary_tree_nodes(tree->left);
+	count_right = binary_tree_nodes(tree->right);
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1 + count_left + count_right);
+	}
+	else
+	{
+		return (count_left + count_right);
+	}
 }
